33c47d70642aee51beebb3450948afce43f71f21
Elhemage <lmh2408@gmail.com>
2019-06-27 01:23:29 +0700

    finally i guess

--- app.js
+++ app.js
@@ -32,13 +32,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-
-server.listen(80);
-
-require('./sockets/sockets')(io);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- public/javascripts/index/webSocket.js
+++ public/javascripts/index/webSocket.js
@@ -1,4 +1,4 @@
-const socket = io.connect('http://localhost');
+const socket = io();
 socket.on('connect', () => {
   console.log('Connected!');
 });
